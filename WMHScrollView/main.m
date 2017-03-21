083a861a0f4a946186ae6addebcadc423af58fcf
Archer <minhang.wang@icloud.com>
2017-03-21 16:39:55 +0800

    Initial Commit

---
+++ WMHScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WMHScrollView
+//
+//  Created by Archer on 2017/3/21.
+//  Copyright © 2017年 jiuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
